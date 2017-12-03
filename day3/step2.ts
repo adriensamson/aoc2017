b9fda372ae73f8a8fdb7d45febbfbf910660ed44
Adrien Samson <adrien@adriensamson.fr>
2017-12-03 14:59:59 +0100

    add matrix

--- day3/step2.ts
+++ day3/step2.ts
@@ -1,11 +1,13 @@
-const grid = new Map();
+import {Matrix} from "../lib";
+
+const grid = new Matrix<number>();
 
 let x = 0, y = 0, r = 0, n = 1;
 let min = Math.pow(2 * r - 1, 2) + 1;
 let max = Math.pow((2 * r + 1), 2);
 let segSize = (max - min + 1) / 4;
 
-grid.set(`${x} ${y}`, 1);
+grid.set(x, y, 1);
 
 while (true) {
     n++;
@@ -24,16 +26,16 @@ while (true) {
     } else {
         y++;
     }
-    const sum = (grid.get(`${x+1} ${y}`) || 0)
-        + (grid.get(`${x+1} ${y+1}`) || 0)
-        + (grid.get(`${x} ${y+1}`) || 0)
-        + (grid.get(`${x-1} ${y+1}`) || 0)
-        + (grid.get(`${x-1} ${y}`) || 0)
-        + (grid.get(`${x-1} ${y-1}`) || 0)
-        + (grid.get(`${x} ${y-1}`) || 0)
-        + (grid.get(`${x+1} ${y-1}`) || 0);
+    const sum = grid.get(x+1, y, 0)
+        + grid.get(x+1, y+1, 0)
+        + grid.get(x, y+1, 0)
+        + grid.get(x-1, y+1, 0)
+        + grid.get(x-1, y, 0)
+        + grid.get(x-1, y-1, 0)
+        + grid.get(x, y-1, 0)
+        + grid.get(x+1, y-1, 0);
+    grid.set(x, y, sum);
     console.log(sum);
-    grid.set(`${x} ${y}`, sum);
     if (sum > 265149) {
         process.exit(0);
     }
--- lib.ts
+++ lib.ts
@@ -7,3 +7,42 @@ export function getContent(filename : string) : string {
 export function getLines(filename : string) : string[] {
     return getContent(filename).split('\n');
 }
+
+export class Matrix<T> {
+    private map  = new Map<string, T>();
+    private minX = 0;
+    private maxX = 0;
+    private minY = 0;
+    private maxY = 0;
+
+    get<U = T>(x : number, y : number, def : T | U = null) : T | U {
+        if (!this.map.has(`${x} ${y}`)) {
+            return def;
+        }
+        return this.map.get(`${x} ${y}`)!;
+    }
+
+    set(x : number, y : number, val : T) {
+        this.map.set(`${x} ${y}`, val);
+        this.minX = Math.min(this.minX, x);
+        this.maxX = Math.max(this.maxX, x);
+        this.minY = Math.min(this.minY, y);
+        this.maxY = Math.max(this.maxY, y);
+    }
+
+    toString() {
+        let str = '';
+        for (let y = this.minY; y <= this.maxY; y++) {
+            for (let x = this.minX; x <= this.maxX; x++) {
+                str += this.get(x, y, ' ');
+                if (x < this.maxX) {
+                    str += '\t';
+                }
+            }
+            if (y < this.maxY) {
+                str += '\n';
+            }
+        }
+        return str;
+    }
+}
